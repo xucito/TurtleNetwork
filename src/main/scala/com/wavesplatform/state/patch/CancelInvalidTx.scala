816307aaa66d7746b25523bbc2d003024b142aa0
bnagtega <bram.nagtegals@trendminer.com>
2018-12-27 11:11:46 +0100

    [add wrong negative sign]

--- src/main/scala/com/wavesplatform/state/patch/CancelInvalidTx.scala
+++ src/main/scala/com/wavesplatform/state/patch/CancelInvalidTx.scala
@@ -22,10 +22,10 @@ object CancelInvalidTx extends ScorexLogging {
 
     val diff = s.collectLposPortfolios {
       case (addr, p) if addr == addr1 && bal1 != 0L =>
-        Portfolio(p.balance, LeaseBalance(0L, 0L), Map.empty)
+        Portfolio(-p.balance, LeaseBalance(0L, 0L), Map.empty)
 
       case (addr, p) if addr == addr2 && bal2 != 0L =>
-        Portfolio(p.balance, LeaseBalance(0L, 0L), Map.empty)
+        Portfolio(-p.balance, LeaseBalance(0L, 0L), Map.empty)
 
       case (addr, p) if addr == addr3 =>
         Portfolio(bal1 + bal2, p.lease, Map.empty)
