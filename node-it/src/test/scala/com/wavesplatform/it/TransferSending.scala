5a0d52305f437c1b7bf4a7451ace8a2685ac7cf9
bnagtega <bram.nagtegals@trendminer.com>
2019-10-07 19:15:38 +0200

    fix fees for random addr generation so Generate transactions and wait for one block with 2000 txs

--- node-it/src/test/scala/com/wavesplatform/it/TransferSending.scala
+++ node-it/src/test/scala/com/wavesplatform/it/TransferSending.scala
@@ -80,7 +80,7 @@ trait TransferSending extends ScorexLogging {
   }
 
   def generateTransfersToRandomAddresses(n: Int, excludeSrcAddresses: Set[String]): Seq[Req] = {
-    val fee      = 100000
+    val fee      = 2000000
     val seedSize = 32
 
     val seeds = NodeConfigs.Default.collect {
